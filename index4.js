97112bb893902e2b5db1c80eb649025460f0187f
jinqiwen <15256221809jin>
2018-05-19 22:22:40 +0800

    完善页面跳转和上下导航切换

--- index4.js
+++ index4.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { AppRegistry,Component } from 'react-native';
 import { Button, View, Text } from 'react-native';
-import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json11
 import app2 from './App2'
 
 AppRegistry.registerComponent('ReactNative1', () => app2);
